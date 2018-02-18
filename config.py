ea6d24b0d9e4f94b2b49a9f1b899135054a8a035
ajaaibu <ajaaibu@gmail.com>
2018-02-18 13:03:56 +0500

    Config mapping of probes corrected

--- config.py
+++ config.py
@@ -2,12 +2,12 @@ config = {
     # Mapping of DS18B20 temperature probes.
     # Please set a name, serial number must only contain serial of the probe starting with 28-
     "DS18B20" : [
-        # {
-        #     'name': 'Nutition Tank',
-        #     'serial': '28-031685612cff'
-        # },
         {
             'name': 'Water Tank',
+            'serial': '28-031685612cff'
+        },
+        {
+            'name': 'Nutrition Tank',
             'serial': '28-041685d1d1ff'
         }
     ],
