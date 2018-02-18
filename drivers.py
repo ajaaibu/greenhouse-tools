c61f659ffe2cc57d4fbc6ef1de66084ff2b1995f
ajaaibu <ajaaibu@gmail.com>
2018-02-18 12:39:29 +0500

    Fixes

--- drivers.py
+++ drivers.py
@@ -19,6 +19,6 @@ def TempProbe(name, serial):
 def DHT22(name, pin):
     try:
         h,t = dht.read_retry(dht.DHT22, pin)
-        return {"temperature": ('{:3.2f}'.format(t)), "humidity": ('{:3.2f}'.format(h))}
+        return {"temperature": ('{0:0.1f}'.format(t)), "humidity": ('{0:0.1f}'.format(h))}
     except:
         return 'Couldn\'t read from %s, pin: %s' % (name, pin)
